23f7936c14ff644df866b0edd9a5476e0a222eb1
soni801 <72191139+jegkjedermeg@users.noreply.github.com>
2021-03-23 13:56:10 +0100

    Initial Commit

---
+++ Kaffe Diem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kaffe Diem
+//
+//  Created by Samuel Aasen on 23/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kaffe Diem/Kaffe_DiemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kaffe_DiemApp.swift
+//  Kaffe Diem
+//
+//  Created by Samuel Aasen on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Kaffe_DiemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
